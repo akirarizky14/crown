e812bbec14a06d310f54bb8897a9ae39996da0dd
akr1405 <akirarizkyyy@gmail.com>
2024-04-15 23:29:45 +0700

    fix: config website

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
+        {/* <Navbar/> */}
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Carousel from '../../components/Carousel/Carousel'
-
+import React from 'react';
+import Countdown from './Countdown';
+import '../../css/views/Home/Home.css';
+import logo from '../../assets/logo_crown_car.png';
 function Home() {
   return (
-    <>
-        <Carousel/>
-    </>
-  )
+    <div className="home-container">
+      <img src={logo} alt="Logo Crown Car" />
+      <p className="home-text">Saat ini Website Crown Car sedang dalam perbaikan</p>
+      <Countdown />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
