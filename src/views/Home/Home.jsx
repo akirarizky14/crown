aef0ba01906c8865d70e7343f4a34e588ca43c55
akr1405 <akirarizkyyy@gmail.com>
2024-04-19 15:03:04 +0700

    step

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Carousel from '../../components/Carousel/Carousel'
 import Explore from '../../components/Explore/Explore'
+import Steps from '../../components/Steps/Steps'
 
 function Home() {
   return (
     <>
         <Carousel/>
         <Explore/>
+        <Steps/>
     </>
   )
 }
